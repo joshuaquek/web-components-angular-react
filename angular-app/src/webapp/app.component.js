0306d99690c8b09dc63bf769bbae868bbbc8e82e
Joshua Quek <joven2468@gmail.com>
2019-05-21 15:47:48 +0800

    Minor refactoring

--- angular-app/src/webapp/app.component.js
+++ angular-app/src/webapp/app.component.js
@@ -1,20 +1,20 @@
 export class AppComponent {
+    hello = 'Angular Web Component';
+
+    static $inject = ['$http', 'config'];
+
+    constructor ($http, config) {
+      this.$http = $http
+      this.config = config
+    }
+
     static component = {
-        template: `
+      template: `
             <div>
                 <h3>Title: {{ $ctrl.config.title }}</h3>
                 <p>Greeting: {{ $ctrl.hello }}</p>
             </div>
         `,
-        controller: AppComponent,
-    };
-
-    static $inject = ['$http', 'config'];
-
-    hello = 'Angular Web Component';
-
-    constructor($http, config) {
-        this.$http = $http;
-        this.config = config;
+      controller: AppComponent
     }
 }
